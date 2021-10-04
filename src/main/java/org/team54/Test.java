9f9c422b65711e7df67601f5aef2e6dcdaa915c7
Blackmesa-Canteen <blackmesa_canteen@riseup.net>
2021-10-04 15:57:33 +0800

    Initial commit

---
+++ src/main/java/org/team54/Test.java
@@ -0,0 +1,13 @@
+package org.team54;
+
+/**
+ * @author Xiaotian
+ * @program distributed-ass2-Decentralized-Chat
+ * @description
+ * @create 2021-10-04 15:47
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
